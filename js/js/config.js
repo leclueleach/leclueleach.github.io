5029aba69361b3b0040659207582277c5fe8848e
leclueleach <lecluesemail@gmail.com>
2025-11-11 17:18:43 +0200

    Delete js/js directory

--- js/js/config.js
+++
@@ -1,4 +0,0 @@
-
-// Rename this to config.js with your public anon credentials (SELECT-only RLS).
-export const SUPABASE_URL = "https://mwasxsyfowbciwhbrbmx.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13YXN4c3lmb3diY2l3aGJyYm14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NzY4ODAsImV4cCI6MjA3ODM1Mjg4MH0.d76mOXeX3ZP2F_-X36FRSOshO2W-AVyJTHTOJY6VJlg";
